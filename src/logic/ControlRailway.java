fa3cce020e1f075ce03d902f97b9e6ea78a85558
gsanyas <guillem.sanyas@gmail.com>
2020-01-24 14:27:20 +0100

    Added comments to the railway class

--- src/logic/ControlRailway.java
+++ src/logic/ControlRailway.java
@@ -5,38 +5,96 @@ import model.Station;
 import model.Train;
 import view.Window;
 
+/**
+ * 
+ * The complete railway system
+ * 
+ * @author Santos Daniel
+ * 		   Sanyas Guillem
+ *
+ */
 public class ControlRailway {
+	
+	/**
+	 * The lines of the railway. A line is an abstract structure representing
+	 * two stations and the rails in between, with a given fixed direction.
+	 */
 	private Line[] lines;
+	
+	/**
+	 * The window in which the railway will be displayed.
+	 */
 	private Window window;
+	
+	/**
+	 * The trains in the railway. Each one is a thread.
+	 */
 	private Train[] trains;
 	
-	public ControlRailway() {
-		
+	
+	
+	/**
+	 * Constructor for the railway
+	 */
+	public ControlRailway() {	
 	}
+	
+	
+	
+	/**
+	 * Replaces the trains in the railway with the trains given
+	 * 
+	 * @param _trains : the new trains in the railway
+	 */
 	public void addTrains( Train..._trains) {
 		trains = _trains;
 	}
 	
+	/**
+	 * Replaces the lines in the railway with the lines given
+	 * 
+	 * @param _lines : the new lines in the railway
+	 */
 	public void addLines( Line..._lines ) {
 		lines = _lines;
+		// Information for the display
 		for( int i= 0; i< trains.length; i++) {	
 			trains[i].getCord().setY(180+i*55);
 		}
 	}
 	
-	
+	/**
+	 * Getter of the displaying window
+	 * 
+	 * @return the window in which the railway is displayed
+	 */
 	public Window getWindow() {
 		return window;
 	}
 	
+	/**
+	 * Getter of the lines.
+	 * 
+	 * @return an array containing the lines
+	 */
 	public Line[] getLines() {
 		return lines;
 	}
 	
+	/**
+	 * Method used by trains to determine the next line they will run through.
+	 * 
+	 * @param l : the current line in which the train is.
+	 * @param s : the current direction of the train.
+	 * @return the next line.
+	 */
 	public Line getNextLine( Line l, Direction s) {
 		Line res = null;
+		// search for the current line in the entire table
 		for( int i= 0; i< lines.length; i++) {
+			// When the current line is found : happens only once
 			if( lines[i] == l) {
+				// depending on direction gives the line on left or right
 				res = s == Direction.LR? lines[i+1] : lines[i-1];
 			}
 		}
